6373c6fe6e80ec460132510f9eec4667225e39d1
Vijaya Strikar PORALLA <vijayastrikarporalla@L20358.local>
2025-04-23 17:38:26 +0200

    fix puppeteer path

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.get('/api/exchange-rate', async (req, res) => {
     const browser = await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: puppeteer.executablePath()
     });
     // console.log('Browser launched');
     
